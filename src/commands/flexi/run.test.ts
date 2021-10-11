d125377821ade35a0cda19b1d96baf5a616ba2ea
gitfish <mfisher.au@gmail.com>
2021-10-11 19:48:01 +1100

    Added export test.

--- src/commands/flexi/run.test.ts
+++ src/commands/flexi/run.test.ts
@@ -149,4 +149,67 @@ describe('flexi:script', () => {
         expect(runContext.flags.path).toBe('test.ts');
         expect(runContext.flags.targetusername).toBe('woo@test.com');
     });
+
+    test('ts script with export', async () => {
+        const projectPath = `${pathUtils.sep}test-project`;
+
+        const mockResolveProject = jest.fn();
+        mockResolveProject.mockResolvedValue({
+            getPath() {
+                return projectPath;
+            }
+        });
+
+        SfdxProject.resolve = mockResolveProject;
+
+        const mockOrgCreate = jest.fn();
+        mockOrgCreate.mockResolvedValue({
+            getOrgId() {
+                return 'test-org-id';
+            }
+        });
+
+        Org.create = mockOrgCreate;
+
+        FileServiceRef.current = {
+            async pathExists() {
+                return true;
+            },
+            readFile: throwInvalidCall,
+            mkdir: throwInvalidCall,
+            readdir: throwInvalidCall,
+            unlink: throwInvalidCall,
+            writeFile: throwInvalidCall
+        };
+
+        let requiredId: string;
+        let runContext: SfdxRunContext;
+        let tsNodeRegisterOpts;
+        RequireFunctionRef.current = (id: string) => {
+            if (id === 'ts-node') {
+                return {
+                    register(opts) {
+                        tsNodeRegisterOpts = opts;
+                    }
+                };
+            }
+            requiredId = id;
+            return {
+                main: (context: SfdxRunContext) => {
+                    runContext = context;
+                }
+            };
+        };
+
+        await RunCommand.run(['--path', 'test.ts', '--export', 'main', '--targetusername', 'woo@test.com']);
+
+        expect(tsNodeRegisterOpts).toBeTruthy();
+        expect(requiredId).toBe(pathUtils.join(projectPath, 'test.ts'));
+        expect(runContext).toBeTruthy();
+        expect(runContext.org.getOrgId()).toBe('test-org-id');
+        expect(runContext.project.getPath()).toBe(projectPath);
+        expect(runContext.flags.path).toBe('test.ts');
+        expect(runContext.flags.export).toBe('main');
+        expect(runContext.flags.targetusername).toBe('woo@test.com');
+    });
 });
